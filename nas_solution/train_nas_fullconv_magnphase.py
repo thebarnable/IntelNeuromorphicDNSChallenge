32bb69f9b1fc94f928b2f6de717158bd2119668a
johnson.loh <johnson.loh@rwth-aachen.de>
2023-09-12 11:29:34 +0200

    final state

---
+++ nas_solution/train_nas_fullconv_magnphase.py
@@ -0,0 +1,438 @@
+# Copyright (C) 2021-22 Intel Corporation
+# SPDX-License-Identifier: MIT
+# See: https://spdx.org/licenses/
+
+import os
+import h5py
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from lava.lib.dl import slayer
+from audio_dataloader import DNSAudio
+from snr import si_snr
+
+from datetime import datetime
+#ct_str = datetime.now().strftime("%Y%m%d%H%M%S")
+ct_str = datetime.now().strftime("%Y%m%d")
+
+import pdb
+import itertools
+
+def collate_fn(batch):
+    noisy, clean, noise = [], [], []
+
+    for sample in batch:
+        noisy += [torch.FloatTensor(sample[0])]
+        clean += [torch.FloatTensor(sample[1])]
+        noise += [torch.FloatTensor(sample[2])]
+
+    return torch.stack(noisy), torch.stack(clean), torch.stack(noise)
+
+
+def stft_splitter(audio, n_fft=512):
+    with torch.no_grad():
+        audio_stft = torch.stft(audio,
+                                n_fft=n_fft,
+                                onesided=True,
+                                return_complex=True)
+        return audio_stft.abs(), audio_stft.angle()
+
+
+def stft_mixer(stft_abs, stft_angle, n_fft=512):
+    return torch.istft(torch.complex(stft_abs * torch.cos(stft_angle),
+                                        stft_abs * torch.sin(stft_angle)),
+                        n_fft=n_fft, onesided=True)
+
+
+class Network(torch.nn.Module):
+    def __init__(self,k,c,d,  threshold=0.1, tau_grad=0.1, scale_grad=0.8, max_delay=64, out_delay=0):
+        super().__init__()
+        self.stft_mean = 0.2
+        self.stft_var = 1.5
+        self.stft_max = 140
+        self.out_delay = out_delay
+
+        self.blocks = torch.nn.ModuleList([
+            torch.nn.Conv1d(2*257, c, kernel_size=k, padding='same', bias=False),
+            torch.nn.BatchNorm1d(c),
+            torch.nn.ReLU(inplace=True),
+        ])
+
+        for ii in range(d):
+            self.blocks.append(torch.nn.Conv1d(c, c, kernel_size=k, padding='same', bias=False))
+            self.blocks.append(torch.nn.BatchNorm1d(c))
+            self.blocks.append(torch.nn.ReLU(inplace=True))
+
+        self.blocks.append(torch.nn.Conv1d(c, 2*257, kernel_size=k, padding='same', bias=False))
+        self.blocks.append(torch.nn.BatchNorm1d(2*257))
+        self.blocks.append(torch.nn.ReLU(inplace=True))
+
+    def forward(self, noisy):
+        # pdb.set_trace()
+        x = noisy
+        for block in self.blocks:
+            x = block(x)
+
+        return x
+
+    def validate_gradients(self):
+        valid_gradients = True
+        for name, param in self.named_parameters():
+            if param.grad is not None:
+                valid_gradients = not (torch.isnan(param.grad).any()
+                                       or torch.isinf(param.grad).any())
+                if not valid_gradients:
+                    break
+        if not valid_gradients:
+            self.zero_grad()
+
+    def export_hdf5(self, filename):
+        # network export to hdf5 format
+        h = h5py.File(filename, 'w')
+        layer = h.create_group('layer')
+        for i, b in enumerate(self.blocks):
+            b.export_hdf5(layer.create_group(f'{i}'))
+
+        
+def nop_stats(dataloader, stats, sub_stats, print=True):
+    t_st = datetime.now()
+    for i, (noisy, clean, noise) in enumerate(dataloader):
+        with torch.no_grad():
+            noisy = noisy
+            clean = clean
+
+            score = si_snr(noisy, clean)
+            sub_stats.correct_samples += torch.sum(score).item()
+            sub_stats.num_samples += noisy.shape[0]
+
+            processed = i * dataloader.batch_size
+            total = len(dataloader.dataset)
+            time_elapsed = (datetime.now() - t_st).total_seconds()
+            samples_sec = time_elapsed / (i + 1) / dataloader.batch_size
+            header_list = [f'Train: [{processed}/{total} '
+                           f'({100.0 * processed / total:.0f}%)]']
+            if print:
+                stats.print(0, i, samples_sec, header=header_list)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-gpu',
+                        type=int,
+                        default=[0],
+                        help='which gpu(s) to use', nargs='+')
+    parser.add_argument('-b',
+                        type=int,
+                        default=64,
+                        help='batch size for dataloader')
+    parser.add_argument('-lr',
+                        type=float,
+                        default=0.01,
+                        help='initial learning rate')
+    parser.add_argument('-lam',
+                        type=float,
+                        default=0.001,
+                        help='lagrangian factor')
+    parser.add_argument('-threshold',
+                        type=float,
+                        default=0.1,
+                        help='neuron threshold')
+    parser.add_argument('-tau_grad',
+                        type=float,
+                        default=0.1,
+                        help='surrogate gradient time constant')
+    parser.add_argument('-scale_grad',
+                        type=float,
+                        default=0.8,
+                        help='surrogate gradient scale')
+    parser.add_argument('-n_fft',
+                        type=int,
+                        default=512,
+                        help='number of FFT specturm, hop is n_fft // 4')
+    parser.add_argument('-dmax',
+                        type=int,
+                        default=64,
+                        help='maximum axonal delay')
+    parser.add_argument('-out_delay',
+                        type=int,
+                        default=0,
+                        help='prediction output delay (multiple of 128)')
+    parser.add_argument('-clip',
+                        type=float,
+                        default=10,
+                        help='gradient clipping limit')
+    # parser.add_argument('-exp',
+    #                     type=str,
+    #                     default='',
+    #                     help='experiment differentiater string')
+    parser.add_argument('-seed',
+                        type=int,
+                        default=None,
+                        help='random seed of the experiment')
+    parser.add_argument('-epoch',
+                        type=int,
+                        default=200,
+                        help='number of epochs to run')
+    parser.add_argument('-path',
+                        type=str,
+                        default='../../data/MicrosoftDNS_4_ICASSP/',
+                        help='dataset path')
+
+    args = parser.parse_args()
+
+    # identifier = args.exp
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+        # identifier += '_{}{}'.format(args.optim, args.seed)
+
+    exp_prefix = ct_str + '_loh_nas_runs/'
+    os.makedirs(exp_prefix, exist_ok=True)
+    tensorboard_path = '/mnt/data4tb/stadtmann/dns_challenge_4/tensorboard_logs/' + exp_prefix
+    os.makedirs(tensorboard_path, exist_ok=True)
+    #os.chmod(tensorboard_path,0o777)
+
+    with open(exp_prefix + 'args.txt', 'wt') as f:
+        for arg, value in sorted(vars(args).items()):
+            f.write('{} : {}\n'.format(arg, value))
+    
+    print(f'##### Start NAS run #####')
+
+    kernel_sizes = [5]
+    channel_sizes = [256,512,1024,2048]
+    nn_depth = [2,3,4]
+
+
+    
+
+    kk = 5
+    cc = 256
+    dd = 5
+
+    max_epoch_per_run = 10
+
+    # opt_fcns = [torch.optim.Adadelta,
+    #            torch.optim.Adagrad,
+    #            torch.optim.Adam,
+    #            torch.optim.AdamW,
+    #            torch.optim.SparseAdam,
+    #            torch.optim.Adamax,
+    #            torch.optim.ASGD,
+    #            #torch.optim.LBFGS,
+    #            torch.optim.NAdam,
+    #            torch.optim.RAdam,
+    #            torch.optim.RMSprop,
+    #            torch.optim.Rprop,
+    #            torch.optim.SGD]
+    # lrs = [0.1,0.01,0.001,0.0001,0.00001]
+
+    # for opt_fcn,lr in list(itertools.product(opt_fcns,lrs)):
+
+    for kk,cc,dd in list(itertools.product(kernel_sizes,channel_sizes,nn_depth)):
+
+        cfg_str = 'k' + str(kk) + 'c' + str(cc) + 'd' + str(dd) + '_optfcn' + '_Adam' + '_absarg'
+
+        print(f'##### Start config {cfg_str} #####')
+        
+        trained_folder = exp_prefix + 'trained_' + cfg_str
+        #logs_folder = exp_prefix + 'logs_' + cfg_str
+        print(trained_folder)
+        writer = SummaryWriter(tensorboard_path + cfg_str + '/')
+        #os.chmod(tensorboard_path + cfg_str + '/',0o777)
+
+        os.makedirs(trained_folder, exist_ok=True)
+        #os.makedirs(logs_folder, exist_ok=True)
+
+        lam = args.lam
+
+        print('Using GPUs {}'.format(args.gpu))
+        device = torch.device('cuda:{}'.format(args.gpu[0]))
+
+        out_delay = args.out_delay
+        if len(args.gpu) == 1:
+            net = Network(kk,cc,dd,
+                        args.threshold,
+                        args.tau_grad,
+                        args.scale_grad,
+                        args.dmax,
+                        args.out_delay).to(device)
+            module = net
+        else:
+            net = torch.nn.DataParallel(Network(kk,cc,dd,
+                                                args.threshold,
+                                                args.tau_grad,
+                                                args.scale_grad,
+                                                args.dmax,
+                                                args.out_delay).to(device),
+                                        device_ids=args.gpu)
+            module = net.module
+
+        # Define optimizer module.
+        # optimizer = torch.optim.RAdam(net.parameters(),
+        #                             lr=args.lr,
+        #                             weight_decay=0e-5)
+
+        optimizer = torch.optim.Adam(net.parameters(),lr=0.001)
+        #scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50,100,150], gamma=0.1)
+
+        train_set = DNSAudio(root=args.path + 'training_set/')
+        validation_set = DNSAudio(root=args.path + 'validation_set/')
+
+        train_loader = DataLoader(train_set,
+                                batch_size=args.b,
+                                shuffle=True,
+                                collate_fn=collate_fn,
+                                num_workers=4,
+                                pin_memory=True)
+        validation_loader = DataLoader(validation_set,
+                                    batch_size=args.b,
+                                    shuffle=True,
+                                    collate_fn=collate_fn,
+                                    num_workers=4,
+                                    pin_memory=True)
+
+        base_stats = slayer.utils.LearningStats(accuracy_str='SI-SNR',
+                                                accuracy_unit='dB')
+
+        # print()
+        # print('Base Statistics')
+        # nop_stats(train_loader, base_stats, base_stats.training)
+        # nop_stats(validation_loader, base_stats, base_stats.validation)
+        # print()
+
+        stats = slayer.utils.LearningStats(accuracy_str='SI-SNR',
+                                        accuracy_unit='dB')
+
+        for epoch in range(max_epoch_per_run):
+            t_st = datetime.now()
+            for i, (noisy, clean, noise) in enumerate(train_loader):
+                net.train()
+                noisy = noisy.to(device)
+                clean = clean.to(device)
+
+                noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft)
+                clean_abs, clean_arg = stft_splitter(clean, args.n_fft)
+
+                clean_absarg = torch.concat((clean_abs,clean_arg),dim=1)
+                noisy_absarg = torch.concat((noisy_abs,noisy_arg),dim=1)
+
+                denoised_absarg = net(noisy_absarg)
+
+                denoised_abs = denoised_absarg[:,:noisy_abs.shape[1],:]
+                denoised_arg = denoised_absarg[:,noisy_abs.shape[1]:,:]
+
+                clean_rec = stft_mixer(denoised_abs, denoised_arg, args.n_fft)
+
+                score = si_snr(clean_rec, clean)
+                loss = lam * F.mse_loss(denoised_absarg, clean_absarg) + (100 - torch.mean(score))
+
+                assert torch.isnan(loss) == False
+
+                optimizer.zero_grad()
+                loss.backward()
+                net.validate_gradients()
+                torch.nn.utils.clip_grad_norm_(net.parameters(), args.clip)
+                optimizer.step()
+
+                # if i < 10:
+                #     net.grad_flow(path=trained_folder + '/')
+
+                if torch.isnan(score).any():
+                    score[torch.isnan(score)] = 0
+
+                stats.training.correct_samples += torch.sum(score).item()
+                stats.training.loss_sum += loss.item()
+                stats.training.num_samples += noisy.shape[0]
+
+                processed = i * train_loader.batch_size
+                total = len(train_loader.dataset)
+                time_elapsed = (datetime.now() - t_st).total_seconds()
+                samples_sec = time_elapsed / (i + 1) / train_loader.batch_size
+                # header_list = [f'Train: [{processed}/{total} '
+                #             f'({100.0 * processed / total:.0f}%)]']
+                # stats.print(epoch, i, samples_sec, header=header_list)
+
+                #print(f'Epoch: {epoch}, It: {i}, samples_sec: {samples_sec}, loss: {stats.training.loss}, si-snr: {stats.training.accuracy}')
+
+            t_st = datetime.now()
+            for i, (noisy, clean, noise) in enumerate(validation_loader):
+                net.eval()
+
+                with torch.no_grad():
+                    noisy = noisy.to(device)
+                    clean = clean.to(device)
+                    
+                    # noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft)
+                    # clean_abs, clean_arg = stft_splitter(clean, args.n_fft)
+
+                    # denoised_abs = net(noisy_abs)
+                    # noisy_arg = slayer.axon.delay(noisy_arg, out_delay)
+                    # clean_abs = slayer.axon.delay(clean_abs, out_delay)
+                    # clean = slayer.axon.delay(clean, args.n_fft // 4 * out_delay)
+
+                    # clean_rec = stft_mixer(denoised_abs, noisy_arg, args.n_fft)
+                    
+                    # score = si_snr(clean_rec, clean)
+                    # loss = lam * F.mse_loss(denoised_abs, clean_abs) + (100 - torch.mean(score))
+
+                    noisy_abs, noisy_arg = stft_splitter(noisy, args.n_fft)
+                    clean_abs, clean_arg = stft_splitter(clean, args.n_fft)
+
+                    clean_absarg = torch.concat((clean_abs,clean_arg),dim=1)
+                    noisy_absarg = torch.concat((noisy_abs,noisy_arg),dim=1)
+
+                    denoised_absarg = net(noisy_absarg)
+
+                    denoised_abs = denoised_absarg[:,:noisy_abs.shape[1],:]
+                    denoised_arg = denoised_absarg[:,noisy_abs.shape[1]:,:]
+
+                    clean_rec = stft_mixer(denoised_abs, denoised_arg, args.n_fft)
+
+                    score = si_snr(clean_rec, clean)
+                    loss = lam * F.mse_loss(denoised_absarg, clean_absarg) + (100 - torch.mean(score))
+
+
+                    stats.validation.correct_samples += torch.sum(score).item()
+                    stats.validation.loss_sum += loss.item()
+                    stats.validation.num_samples += noisy.shape[0]
+
+                    processed = i * validation_loader.batch_size
+                    total = len(validation_loader.dataset)
+                    time_elapsed = (datetime.now() - t_st).total_seconds()
+                    samples_sec = time_elapsed / \
+                        (i + 1) / validation_loader.batch_size
+                    # header_list = [f'Valid: [{processed}/{total} '
+                    #             f'({100.0 * processed / total:.0f}%)]']
+                    # stats.print(epoch, i, samples_sec, header=header_list)
+
+                    #print(f'Epoch: {epoch}, It: {i}, samples_sec: {samples_sec}, loss: {stats.validation.loss}, si-snr: {stats.validation.accuracy}')
+
+            writer.add_scalar('Loss/train', stats.training.loss, epoch)
+            writer.add_scalar('Loss/valid', stats.validation.loss, epoch)
+            writer.add_scalar('SI-SNR/train', stats.training.accuracy, epoch)
+            writer.add_scalar('SI-SNR/valid', stats.validation.accuracy, epoch)
+
+            print(f'Epoch: {epoch}, loss: {stats.validation.loss}, si-snr: {stats.validation.accuracy}')
+
+            stats.update()
+            stats.plot(path=trained_folder + '/')
+            if stats.validation.best_accuracy is True:
+                torch.save(module.state_dict(), trained_folder + '/network.pt')
+            stats.save(trained_folder + '/')
+
+            #scheduler.step()
+
+        net.load_state_dict(torch.load(trained_folder + '/network.pt'))
+        #net.export_hdf5(trained_folder + '/network.net')
+
+        params_dict = {}
+        for key, val in args._get_kwargs():
+            params_dict[key] = str(val)
+        #writer.add_hparams(params_dict, {'SI-SNR': stats.validation.max_accuracy})
+        writer.flush()
+        writer.close()
